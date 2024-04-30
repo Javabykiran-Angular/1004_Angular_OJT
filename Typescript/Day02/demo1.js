f548622ff705cdfd8d984134f6b1aad71677e50a
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-04-30 17:27:28 +0530

    Day02

---
+++ Typescript/Day02/demo1.js
@@ -0,0 +1,3 @@
+console.log("Hello World...");
+console.log('Welcome to Angular 16 Module...');
+console.log("\n            Hopes so U all are understand...");
---
+++ Typescript/Day02/demo1.ts
@@ -0,0 +1,15 @@
+console.log("Hello World...");
+console.log('Welcome to Angular 16 Module...')
+console.log(`
+            Hopes so U all are understand...`);
+
+var a1,a2;
+var a_1,a$1;
+
+// data type
+// number(int,float,double,long,short),string(''/""),boolean(true/false)
+// any => (number,string,boolean)
+// void => it is neutral data type => it can not store any value 
+// Misec. null & undefined => it works as data type as well as value 
+
+
