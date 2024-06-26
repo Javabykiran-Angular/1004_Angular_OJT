91ab4e115c51534047d0bd1f423610fdfa39abc2
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-06-26 17:33:23 +0530

    Day27

---
+++ Angular/Day27/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+ 
+  ngOnInit(): void {
+   document.body.className="bg_background"; 
+  }
+  
+
+}
